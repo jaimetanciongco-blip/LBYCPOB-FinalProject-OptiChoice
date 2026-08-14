b95d2df22209942209758f5814d2efe1236f8e0f
jaimetanciongco-blip <jaime_tanciongco@dlsu.edu.ph>
2026-08-14 21:35:48 +0800

    Add Spring Boot project files

---
+++ src/main/java/ph/edu/dlsu/lbycpob/optichoice/OptichoiceApplication.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.optichoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptichoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptichoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ph/edu/dlsu/lbycpob/optichoice/OptichoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.optichoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptichoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
