d9241001f5ee906b5514d11376bbc2515f92af10
jaimetanciongco-blip <jaime_tanciongco@dlsu.edu.ph>
2026-08-15 14:48:06 +0800

    created DecisionArchive model to save past evaluations

--- src/main/java/ph/edu/dlsu/lbycpob/optichoice/model/DecisionArchive.java
+++ src/main/java/ph/edu/dlsu/lbycpob/optichoice/model/DecisionArchive.java
@@ -1,4 +1,61 @@
 package ph.edu.dlsu.lbycpob.optichoice.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "decision_archive")
+
 public class DecisionArchive {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String domainName;
+    private String optionA;
+    private String optionB;
+    private String winningChoice;
+    private double winningScore;
+    private LocalDateTime decidedAt;
+
+    public DecisionArchive() {
+    }
+
+    public DecisionArchive(String domainName, String optionA, String optionB, String winningChoice, double winningScore) {
+        this.domainName = domainName;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.winningChoice = winningChoice;
+        this.winningScore = winningScore;
+        this.decidedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public String getWinningChoice() {
+        return winningChoice;
+    }
+
+    public double getWinningScore() {
+        return winningScore;
+    }
+
+    public LocalDateTime getDecidedAt() {
+        return decidedAt;
+
+    }
 }
