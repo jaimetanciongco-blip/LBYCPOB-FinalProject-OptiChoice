4658739e16928266c777f4a0e14d5c701d70b076
jaimetanciongco-blip <jaime_tanciongco@dlsu.edu.ph>
2026-08-15 14:46:29 +0800

    created base abstract DomainCategory class

--- src/main/java/ph/edu/dlsu/lbycpob/optichoice/model/domain/DomainCategory.java
+++ src/main/java/ph/edu/dlsu/lbycpob/optichoice/model/domain/DomainCategory.java
@@ -1,4 +1,25 @@
 package ph.edu.dlsu.lbycpob.optichoice.model.domain;
 
-public class DomainCategory {
+import java.util.List;
+
+public abstract class DomainCategory {
+    private String name;
+    private String description;
+
+    public DomainCategory(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract List<String> getDefaultCriteria();
+    public abstract double calculateWeightedScore(double rawScore, int importanceWeight);
+
 }
