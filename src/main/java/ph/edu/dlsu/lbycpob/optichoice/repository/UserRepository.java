31fb01ffa4a6c767613ff7bc76777bc70addb474
azi <aziel_raymundo@dlsu.edu.ph>
2026-08-15 21:58:50 +0800

    implemented UserRepository

--- src/main/java/ph/edu/dlsu/lbycpob/optichoice/repository/UserRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/optichoice/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package ph.edu.dlsu.lbycpob.optichoice.repository;
 
-public interface UserRepository {
+import ph.edu.dlsu.lbycpob.optichoice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
